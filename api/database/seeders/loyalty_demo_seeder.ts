df6837adb531ceac59924c0638765a9a854f873e
Mateusz <mateusz.aftanski@gmail.com>
2026-08-07 10:28:13 +0200

    feat: enhance loyalty demo seeder with earned rewards and improve readibility

--- api/database/seeders/loyalty_demo_seeder.ts
+++ api/database/seeders/loyalty_demo_seeder.ts
@@ -1,4 +1,5 @@
 import Company from '#models/company'
+import EarnedReward from '#models/earned_reward'
 import LoyaltyAccount from '#models/loyalty_account'
 import LoyaltyProgram from '#models/loyalty_program'
 import NfcTag from '#models/nfc_tag'
@@ -10,9 +11,19 @@ import { DateTime } from 'luxon'
 
 export default class LoyaltyDemoSeeder extends BaseSeeder {
   async run() {
-    const client = { client: this.client }
+    const member = await this.seedMember()
+    const coffeeShop = await this.seedCoffeeShop()
+    const bakery = await this.seedBakery()
 
-    const user = await User.updateOrCreate(
+    const coffeeAccount = await this.seedAccount(member, coffeeShop.program)
+    const bakeryAccount = await this.seedAccount(member, bakery.program)
+
+    await this.seedReward(coffeeAccount, coffeeShop.program)
+    await this.seedStamps(coffeeAccount, bakeryAccount, coffeeShop.tags, bakery.tag)
+  }
+
+  private seedMember() {
+    return User.updateOrCreate(
       { email: 'member@example.com' },
       {
         encryptedPassword: 'password123',
@@ -20,47 +31,32 @@ export default class LoyaltyDemoSeeder extends BaseSeeder {
         phoneE164: '+48501123456',
         phoneVerifiedAt: DateTime.fromISO('2026-08-04T12:00:00Z'),
       },
-      client
+      { client: this.client }
     )
+  }
 
-    const firstCompany = await Company.updateOrCreate(
+  private async seedCoffeeShop() {
+    const company = await Company.updateOrCreate(
       { name: 'Kawiarnia Północ' },
       { name: 'Kawiarnia Północ' },
-      client
+      { client: this.client }
     )
-    const secondCompany = await Company.updateOrCreate(
-      { name: 'Piekarnia Gorunc' },
-      { name: 'Piekarnia Gorunc' },
-      client
-    )
-
-    const firstProgram = await LoyaltyProgram.updateOrCreate(
-      { companyId: firstCompany.id },
+    const program = await LoyaltyProgram.updateOrCreate(
+      { companyId: company.id },
       {
-        companyId: firstCompany.id,
+        companyId: company.id,
         name: 'Kawa na pieczątki',
         rewardTitle: 'Darmowa kawa',
         stampsRequired: 10,
         active: true,
       },
-      client
-    )
-    const secondProgram = await LoyaltyProgram.updateOrCreate(
-      { companyId: secondCompany.id },
-      {
-        companyId: secondCompany.id,
-        name: 'Drożdżówka na pieczątki',
-        rewardTitle: 'Darmowa drożdżówka',
-        stampsRequired: 5,
-        active: true,
-      },
-      client
+      { client: this.client }
     )
 
-    const firstCompanyFirstVenue = await Venue.updateOrCreate(
-      { companyId: firstCompany.id, name: 'Śródmieście' },
+    const downtownVenue = await Venue.updateOrCreate(
+      { companyId: company.id, name: 'Śródmieście' },
       {
-        companyId: firstCompany.id,
+        companyId: company.id,
         name: 'Śródmieście',
         addressLine1: 'ul. Marszałkowska 10',
         postalCode: '00-590',
@@ -69,12 +65,12 @@ export default class LoyaltyDemoSeeder extends BaseSeeder {
         latitude: '52.221617',
         longitude: '21.014272',
       },
-      client
+      { client: this.client }
     )
-    const firstCompanySecondVenue = await Venue.updateOrCreate(
-      { companyId: firstCompany.id, name: 'Stare Miasto' },
+    const oldTownVenue = await Venue.updateOrCreate(
+      { companyId: company.id, name: 'Stare Miasto' },
       {
-        companyId: firstCompany.id,
+        companyId: company.id,
         name: 'Stare Miasto',
         addressLine1: 'ul. Freta 10',
         postalCode: '00-227',
@@ -83,12 +79,54 @@ export default class LoyaltyDemoSeeder extends BaseSeeder {
         latitude: '52.251623',
         longitude: '21.010097',
       },
-      client
+      { client: this.client }
     )
-    const secondCompanyVenue = await Venue.updateOrCreate(
-      { companyId: secondCompany.id, name: 'Mokotów' },
+
+    const downtownTag = await NfcTag.updateOrCreate(
+      { identifier: '041C6432A91190' },
       {
-        companyId: secondCompany.id,
+        venueId: downtownVenue.id,
+        identifier: '041C6432A91190',
+        active: true,
+        lastAcceptedCounter: 2,
+      },
+      { client: this.client }
+    )
+    const oldTownTag = await NfcTag.updateOrCreate(
+      { identifier: '043D6432A91190' },
+      {
+        venueId: oldTownVenue.id,
+        identifier: '043D6432A91190',
+        active: true,
+        lastAcceptedCounter: 1,
+      },
+      { client: this.client }
+    )
+
+    return { program, tags: { downtown: downtownTag, oldTown: oldTownTag } }
+  }
+
+  private async seedBakery() {
+    const company = await Company.updateOrCreate(
+      { name: 'Piekarnia Gorunc' },
+      { name: 'Piekarnia Gorunc' },
+      { client: this.client }
+    )
+    const program = await LoyaltyProgram.updateOrCreate(
+      { companyId: company.id },
+      {
+        companyId: company.id,
+        name: 'Drożdżówka na pieczątki',
+        rewardTitle: 'Darmowa drożdżówka',
+        stampsRequired: 5,
+        active: true,
+      },
+      { client: this.client }
+    )
+    const venue = await Venue.updateOrCreate(
+      { companyId: company.id, name: 'Mokotów' },
+      {
+        companyId: company.id,
         name: 'Mokotów',
         addressLine1: 'ul. Puławska 2',
         postalCode: '02-566',
@@ -97,70 +135,77 @@ export default class LoyaltyDemoSeeder extends BaseSeeder {
         latitude: '52.209124',
         longitude: '21.019510',
       },
-      client
+      { client: this.client }
     )
-
-    const firstTag = await NfcTag.updateOrCreate(
-      { identifier: '041C6432A91190' },
-      {
-        venueId: firstCompanyFirstVenue.id,
-        identifier: '041C6432A91190',
-        active: true,
-        lastAcceptedCounter: 2,
-      },
-      client
-    )
-    const secondTag = await NfcTag.updateOrCreate(
-      { identifier: '043D6432A91190' },
-      {
-        venueId: firstCompanySecondVenue.id,
-        identifier: '043D6432A91190',
-        active: true,
-        lastAcceptedCounter: 1,
-      },
-      client
-    )
-    const thirdTag = await NfcTag.updateOrCreate(
+    const tag = await NfcTag.updateOrCreate(
       { identifier: 'test1' },
       {
-        venueId: secondCompanyVenue.id,
+        venueId: venue.id,
         identifier: 'test1',
         active: true,
         lastAcceptedCounter: 1,
       },
-      client
+      { client: this.client }
     )
 
-    const firstAccount = await LoyaltyAccount.firstOrCreate(
-      { userId: user.id, loyaltyProgramId: firstProgram.id },
-      {},
-      client
-    )
-    const secondAccount = await LoyaltyAccount.firstOrCreate(
-      { userId: user.id, loyaltyProgramId: secondProgram.id },
-      {},
-      client
-    )
+    return { program, tag }
+  }
 
+  private seedAccount(user: User, program: LoyaltyProgram) {
+    return LoyaltyAccount.firstOrCreate(
+      { userId: user.id, loyaltyProgramId: program.id },
+      {},
+      { client: this.client }
+    )
+  }
+
+  private async seedReward(account: LoyaltyAccount, program: LoyaltyProgram) {
+    const rewardEarnedAt = DateTime.fromISO('2026-08-04T12:00:00Z')
+    await EarnedReward.updateOrCreate(
+      { loyaltyAccountId: account.id, earnedAt: rewardEarnedAt },
+      {
+        loyaltyAccountId: account.id,
+        rewardTitleSnapshot: program.rewardTitle,
+        stampsRequiredSnapshot: program.stampsRequired,
+        earnedAt: rewardEarnedAt,
+      },
+      { client: this.client }
+    )
+  }
+
+  private async seedStamps(
+    coffeeAccount: LoyaltyAccount,
+    bakeryAccount: LoyaltyAccount,
+    coffeeTags: { downtown: NfcTag; oldTown: NfcTag },
+    bakeryTag: NfcTag
+  ) {
     await Stamp.updateOrCreate(
-      { nfcTagId: firstTag.id, nfcCounter: 1 },
-      { loyaltyAccountId: firstAccount.id, nfcTagId: firstTag.id, nfcCounter: 1 },
-      client
+      { nfcTagId: coffeeTags.downtown.id, nfcCounter: 1 },
+      {
+        loyaltyAccountId: coffeeAccount.id,
+        nfcTagId: coffeeTags.downtown.id,
+        nfcCounter: 1,
+      },
+      { client: this.client }
     )
     await Stamp.updateOrCreate(
-      { nfcTagId: firstTag.id, nfcCounter: 2 },
-      { loyaltyAccountId: firstAccount.id, nfcTagId: firstTag.id, nfcCounter: 2 },
-      client
+      { nfcTagId: coffeeTags.downtown.id, nfcCounter: 2 },
+      {
+        loyaltyAccountId: coffeeAccount.id,
+        nfcTagId: coffeeTags.downtown.id,
+        nfcCounter: 2,
+      },
+      { client: this.client }
     )
     await Stamp.updateOrCreate(
-      { nfcTagId: secondTag.id, nfcCounter: 1 },
-      { loyaltyAccountId: firstAccount.id, nfcTagId: secondTag.id, nfcCounter: 1 },
-      client
+      { nfcTagId: coffeeTags.oldTown.id, nfcCounter: 1 },
+      { loyaltyAccountId: coffeeAccount.id, nfcTagId: coffeeTags.oldTown.id, nfcCounter: 1 },
+      { client: this.client }
     )
     await Stamp.updateOrCreate(
-      { nfcTagId: thirdTag.id, nfcCounter: 1 },
-      { loyaltyAccountId: secondAccount.id, nfcTagId: thirdTag.id, nfcCounter: 1 },
-      client
+      { nfcTagId: bakeryTag.id, nfcCounter: 1 },
+      { loyaltyAccountId: bakeryAccount.id, nfcTagId: bakeryTag.id, nfcCounter: 1 },
+      { client: this.client }
     )
   }
 }
